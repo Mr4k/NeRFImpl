42137abcd488890549064690e71c456ca61b6e17
Peter Stefek <peterstefek@Peters-MacBook-Pro.local>
2022-12-21 15:36:20 -0500

    .

--- learner.py
+++ learner.py
@@ -1,6 +1,7 @@
+import uuid
 import torch
-from batch_and_sampler import render_rays, sample_batch
-from matrix_math_utils import generate_rot_x, generate_rot_z, generate_translation
+from batch_and_sampler import render_image, render_rays, sample_batch
+from matrix_math_utils import generate_random_gimbal_transformation_matrix, generate_rot_x, generate_rot_z, generate_translation
 from nerf import load_config_file
 
 from neural_nerf import NerfModel
@@ -12,6 +13,12 @@ import os
 
 
 def train():
+    training_run_id = uuid.uuid4()
+    out_dir = f"./training_output/runs/{training_run_id}/"
+
+    print(f"creating output dir: {out_dir}")
+    os.makedirs(out_dir)
+
     # (batch size, 3)
     scale = 1.0 / 5.0
     batch_size = 4096
@@ -49,7 +56,28 @@ def train():
     optimizer = torch.optim.Adam(model.parameters(), 0.0005)
 
     num_steps = 100000
+    num_steps_to_render = 10
     for step in range(num_steps):
+        if step % num_steps_to_render == 0:
+            print(f"rendering snapshot at step {step}")
+            size = 100
+            novel_view_transformation_matrix = generate_random_gimbal_transformation_matrix(1.0/scale)
+            depth_image, color_image = render_image(size, novel_view_transformation_matrix, fov, near, far, model.parameters())
+            imageio.imwrite(
+                out_dir + f"depth_step_{step}.png",
+                (depth_image).reshape((size, size)).t().fliplr().numpy(),
+            )
+            imageio.imwrite(
+                out_dir + f"color_step_{step}.png",
+                (color_image * 255)
+                .reshape((size, size, 3))
+                .transpose(0, 1)
+                .flip([1])
+                .numpy(),
+            )
+            print("saved snapshot")
+
+
         camera_poses, rays, distance_to_depth_modifiers, expected_colors = sample_batch(
             batch_size,
             200,
