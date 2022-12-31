34bc090bdf6622552af7037a1eabe25f43f042c2
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2022-12-31 17:48:13 -0500

    .

--- neural_nerf.py
+++ neural_nerf.py
@@ -7,12 +7,12 @@ points: tensor dim = (num points, space_dim)
 
 def embed_tensor(points, l, device):
     embeddings = points.repeat(1, l)
-    scalers = torch.pow(2, torch.arange(0, l, 1)).repeat_interleave(3).to(device)
+    scalers = torch.pow(2, torch.arange(0, l, 1, device=device)).repeat_interleave(3)
 
     # TODO unknown if this different ordering will be a problem with learning
-    sin_embeddings = torch.sin(embeddings * scalers[None, :] * torch.pi).to(device)
-    cos_embeddings = torch.cos(embeddings * scalers[None, :] * torch.pi).to(device)
-    return torch.concat([sin_embeddings, cos_embeddings], dim=1).to(device)
+    sin_embeddings = torch.sin(embeddings * scalers[None, :] * torch.pi)
+    cos_embeddings = torch.cos(embeddings * scalers[None, :] * torch.pi)
+    return torch.concat([sin_embeddings, cos_embeddings], dim=1)
 
 
 class NerfModel(torch.nn.Module):
