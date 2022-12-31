aa554d2564215d9a389fe36c2d55ec8a9d5cc95f
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2022-12-31 17:57:42 -0500

    .

--- neural_nerf.py
+++ neural_nerf.py
@@ -4,17 +4,6 @@ import torch
 points: tensor dim = (num points, space_dim)
 """
 
-
-def embed_tensor(points, l, device):
-    embeddings = points.repeat(1, l)
-    scalers = torch.pow(2, torch.arange(0, l, 1, device=device)).repeat_interleave(3)
-
-    # TODO unknown if this different ordering will be a problem with learning
-    sin_embeddings = torch.sin(embeddings * scalers[None, :] * torch.pi)
-    cos_embeddings = torch.cos(embeddings * scalers[None, :] * torch.pi)
-    return torch.concat([sin_embeddings, cos_embeddings], dim=1)
-
-
 class NerfModel(torch.nn.Module):
     def __init__(self, scale, device):
         super(NerfModel, self).__init__()
@@ -27,6 +16,11 @@ class NerfModel(torch.nn.Module):
 
         pos_input_dims = self.l_pos * 2 * 3
         dir_input_dims = self.l_dir * 2 * 3
+
+        self.scalers = {
+            pos_input_dims: torch.pow(2, torch.arange(0, pos_input_dims, 1, device=device)).repeat_interleave(3),
+            dir_input_dims: torch.pow(2, torch.arange(0, dir_input_dims, 1, device=device)).repeat_interleave(3)
+        }
 
         self.linear1 = torch.nn.Linear(pos_input_dims, 256)
         self.linear2 = torch.nn.Linear(256, 256)
@@ -43,8 +37,17 @@ class NerfModel(torch.nn.Module):
         self.relu_activation = torch.nn.ReLU()
         self.sigmoid_activation = torch.nn.Sigmoid()
 
+    def embed_tensor(self, points, l):
+        embeddings = points.repeat(1, l)
+    
+        # TODO unknown if this different ordering will be a problem with learning
+        sin_embeddings = torch.sin(embeddings * self.scalers[l][None, :] * torch.pi)
+        cos_embeddings = torch.cos(embeddings * self.scalers[l][None, :] * torch.pi)
+        return torch.concat([sin_embeddings, cos_embeddings], dim=1)
+
     def forward(self, pos_input, dir_input):
-        pos_embedding = embed_tensor(pos_input / self.scale, self.l_pos, self.device)
+        pos_embedding = self.embed_tensor(pos_input / self.scale, self.l_pos, self.device)
+
         x = self.linear1(pos_embedding)
         x = self.relu_activation(x)
         x = self.linear2(x)
