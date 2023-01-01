d88ae71dbc9bedbe5e21b562369678c78640b853
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2022-12-31 19:46:14 -0500

    .

--- nerf.py
+++ nerf.py
@@ -161,7 +161,7 @@ def trace_ray(
 
     # TODO (getting rid of this for loop likely speeds up rendering)
     # on second thought maybe not, bottleneck will eventually likely be get_network_output
-    deltas = stratified_sample_times[:, 1:i + 1] - stratified_sample_times[:, 0:i]
+    deltas = stratified_sample_times[:, 1:-1] - stratified_sample_times[:, 0:-2]
     for i in range(n):
         prob_hit_current_bin = 1 - torch.exp(-opacity[:, i] * deltas[i])
         cum_passthrough_prob = torch.exp(-cum_partial_passthrough_sum)
