78c493229bfbd08686620cdfbfa6870c32d26be7
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2023-01-18 21:37:24 -0500

    .

--- nerf.py
+++ nerf.py
@@ -194,7 +194,7 @@ def trace_ray(
         radiance_field,
         stratified_sample_points.reshape(-1, 3),
         #directions.repeat_interleave(num_samples + 1, dim=0),
-        directions.repeat(num_samples + 1),
+        directions.repeat(num_samples + 1, 1),
     )
     colors = colors.reshape(batch_size, num_samples + 1, 3)[:, 0:-1]
     opacity = opacity.reshape(batch_size, num_samples + 1)#[:, 0:-1]
